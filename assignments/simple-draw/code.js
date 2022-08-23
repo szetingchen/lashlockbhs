0984e7c10b32788b2c864a96015b045122e751b4
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-08-23 15:40:50 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,1 +1,56 @@
-// Test
+
+/*
+var x = 0
+var y = 0
+var size = 250
+for(let i=0; i < 25; i++){
+    drawTriangle(x, y, size, 'red');
+    var size=size-10
+    var x=x+10
+    var y=y+15
+}
+
+ 
+function drawTriangle(x, y, size, color) {
+    if(x <= 0){
+        var x = size
+    }
+    else if(x >= 0){
+        var x = size + x
+    }
+    if(y == 0){
+ 
+    }
+    var px = x
+    var py = y
+    var px1 = x
+    var py1 = y
+    for(let i=0; i < size; i++){
+        var px = px - 1
+        var py = py + 2
+        drawRect(px, py, 1, 1, color);
+    }
+    for(let i=0; i < size; i++){
+        var px1 = px1 + 1
+        var py1 = py1 + 2
+        drawRect(px1, py1, 1, 1, color);
+    }
+    for(let i=0; i < size/3*2; i++){
+        drawRect(px1, py1, 1, 1, color);
+        var px1 = px1 -3
+    }
+ 
+}
+*/
+
+var x = 100
+var y = 100
+var size = 100
+var color = 'black'
+drawTriangle(x, y, size, color)
+function drawTriangle(x, y, size, color){
+    drawLine(x, y, x+size/2, y+size, color)
+    drawLine(x+size/2, y+size, x+size/2-size, y+size, color)
+    drawLine(x, y, x-size/2, y+size, color)
+   
+}
