60a91f5695d84a9638d053d3ba34438b13abc755
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-08-26 14:37:33 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -30,7 +30,7 @@ const drawFallingTriangle = (x, time) => {
   // Figure out the y values as a function of time.
   let y1 = (time / 4) % height;
   let y2 = y1 + 3;
-  let y3 = y1 - 4;
+  let y3 = y1 - 45;
 
   // Actually draw the triangle.
   drawTriangle(x1, y1, x2, y2, x3, y3, 'pink', 1);
