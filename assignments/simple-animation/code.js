8a7cf42c984bf8ed5cfc675fe725ce4b66f95fc5
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-08-29 14:58:01 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -8,7 +8,7 @@ const drawFrame = (time) => {
   clear();
   drawFilledCircle((time *2) % width, height / 2, 2, 'blue');
   drawFallingTriangle(width / 3, time*2);
-  drawCube(20, (time*34) % 10, 10)
+  drawCube(20, (time*2) % 20, 10)
 
 };
 
