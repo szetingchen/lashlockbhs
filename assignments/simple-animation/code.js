bb6c5a5228ad54ec0f4cc3d49b5363f8501883be
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-08-29 15:02:33 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -23,6 +23,7 @@ const drawTriangle = (x1, y1, x2, y2, x3, y3, color, width = 1) => {
 function drawCube(size, x, y){
     drawRect(x, y, size, size, 'black')
     drawRect(x+size/3, y+size/3, size, size, 'black')
+    drawLine(x, y, x, y)
     }
 
 const drawFallingTriangle = (x, time) => {
