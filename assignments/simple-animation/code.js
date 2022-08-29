1e881902fd52e384fd1a2dec479107603c1d7201
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-08-29 15:23:04 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -2,7 +2,7 @@ var e = 1
 const drawFrame = (time) => {
     clear();
     if(time<234){
-        drawCube(234 - time/2, 100, 100) * e
+        drawCube(234 - time/2, 78, 100) * e
     }
     else{
         drawCube(234 - time, 10, 10) * e
