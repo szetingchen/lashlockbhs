55b0dab45adfb40422bdba922fd84bef82c614a4
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-02 15:05:09 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -3,7 +3,7 @@ var e1 = false
 const drawFrame = (time) => {
   clear();
   var x = (time/2) - 500
-  if(x === 500){
+  if(x === 400){
     var back = true
     drawCube(10, (-time/2) + 500, 10)
   }
