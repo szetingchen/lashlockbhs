a3f3d6b465b9e9afbc76dbc738d2bab57a86eb0b
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-02 14:46:42 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -1,33 +1,9 @@
 var e = 1
 var e1 = false
 const drawFrame = (time) => {
-    clear();
-    if(time === 500){
-      var start = -1
-    }
-    else{
-      var start = 1
-    }
-    if(time<234*10){
-          var start = 1
-        }
-        if(!e1){
-            drawCube(234 - time/10*start, time, 98)
-
-        }
-        
-    
-    if(time<234*5){
-        var e1 = true
-        
-    }
-    if(e1){
-        drawCube(time/10, 10, 98)
-    }
+  clear();
+  drawCube(10, 10%time/2, 10)
 }
-
-
-
 function drawCube(size, x, y){
     drawRect(x, y, size, size, 'black')
     drawRect(x+size/3, y+size/3, size, size, 'black')
