dff9582efff7fac7cc1a06c1902501558304aea8
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-06 14:55:42 -0700

    Creating assignments/boolean-birds/code.js

---
+++ assignments/boolean-birds/code.js
@@ -0,0 +1,18 @@
+/*
+ * Important facts about the birds of Booleana.
+ *
+ * Coloration and markings:
+ *
+ *   - Flobby Birds and Bloggy Birds are red.
+ *   - Flibble Birds and Globby Birds are not red.
+ *   - Flobby Birds and Flibble Birds are spotted.
+ *   - Bloggy Birds and Globby Birds are not spotted.
+ *
+ * Diet:
+ *
+ *   - Flobby Birds eat fish, nuts, and worms.
+ *   - Bloggy Birds eat mice, nuts, and worms.
+ *   - Flibble Birds eat fish, mice, and worms.
+ *   - Globby Birds eat fish, mice, and nuts.
+ *
+ */
