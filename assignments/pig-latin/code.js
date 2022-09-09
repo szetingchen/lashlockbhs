53f77e0fa7e89940a80528c07f2dac2571f9c9e2
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-09 15:31:34 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -39,6 +39,6 @@ function pigLatin(str){
 }
 function advancedPigLatin(str){
   var volu1 = str.search(/[aeiou]/)
-  return str.substring(volu1) + str.substring(0, volu1) + String.fromCharCode(119*(volu1.toString().replace(volu1, (10*volu1).toString()))).replace("\u0000", "w").replace("î", "") + "ay" 
+  return str.substring(volu1) + str.substring(0, volu1) + String.fromCharCode(119*(volu1.toString().replace(volu1, (100*volu1).toString()))).replace("\u0000", "w").replace("î", "") + "ay" 
   
 }
