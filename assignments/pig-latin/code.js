1b54bb7ec55308b2004c0bc5443c36b3136b0772
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-09 15:08:32 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -39,6 +39,6 @@ function pigLatin(str){
 }
 function advancedPigLatin(str){
   var volu1 = str.search(/[aeiou]/)
-  return str.substring(volu1) + str.substring(0, volu1) + "w"*1 + "ay" 
+  return str.substring(volu1) + str.substring(0, volu1) + String.fromCharCode(119)*0 + "ay" 
   
 }
