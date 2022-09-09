e2b1f90a22fc9f576c73cc04f9cdbd076e4e3795
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-09 14:33:37 -0700

    Creating assignments/pig-latin/code.js

---
+++ assignments/pig-latin/code.js
@@ -0,0 +1,33 @@
+// A new method you will probably want to use.
+//
+// If s is a string:
+//
+//  s.search(/[aeiou]/) returns the index in s of the first vowel.
+//
+// For instance
+//
+//  const s = 'phone';
+//
+//  s.search(/[aeiou]/) returns 2
+//
+// The funny syntax /[aeiou]/ is called a "regular expression". We'll learn
+// about them in more detail later.
+//
+// To write the advancedPigLatin function you will want to know about a new
+// operator called the "ternary operator". (It's called this because it has
+// three operands.)
+//
+// It looks like:
+//
+//  booleanValue ? value1 : value2
+//
+// And it evaluates to value1 is booleanValue is true and value2 if it is false.
+//
+// For example:
+//
+//  x === 0 ? "Zero" : "Not zero"
+//
+// will evaluate to either "Zero" or "Not zero" depending on whether x === 0.
+//
+// Feel free to play around in the REPL to make sure you understand how this
+// operator works.
