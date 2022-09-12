41d80d8fbada8e8e9fba265a0aeef5d98cbe7089
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-12 15:17:16 -0700

    Creating assignments/images-challenge/code.js

---
+++ assignments/images-challenge/code.js
@@ -0,0 +1,25 @@
+/*
+ * This code is running in an environment the same as simple-draw. Thus you have
+ * two variables that will be helpful.
+ *
+ *  width - the width of the drawing area.
+ *  height - the height of the drawing area.
+ *
+ * And these methods which do the same thing as in simple-draw.
+ *
+ *  drawLine(x1, y1, x2, y2, color, lineWidth)
+ *
+ *  drawCircle(x, y, radius, color, lineWidth=1)
+ *
+ *  drawRect(x, y, w, h, color, lineWidth=1)
+ *
+ *  drawTriangle(x1, y1, x2, y2, x3, y3, color, lineWidth=1)
+ *
+ *  drawFilledCircle(x, y, r, color)
+ *
+ *  drawFilledRect(x, y, width, height, color)
+ *
+ *  drawFilledTriangle(x1, y1, x2, y2, x3, y3, color)
+ *
+ *  clear()
+ */
