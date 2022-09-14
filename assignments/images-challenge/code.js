538f8f5c838138223f7ecd20bc6a8494cb069945
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-14 14:41:51 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -32,7 +32,7 @@ const notreallycurved =(num)=>{
   const side = 500
   let linesD = 0
   while(linesD != num){
-    drawLine(300, 600, 0, 0, "black", 1)
+    drawLine(500, 600, 0, 0, "black", 1)
     linesD+=1
   }
 
