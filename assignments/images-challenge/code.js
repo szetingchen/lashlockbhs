f08e526e06f8e607e6691aff212fbe5a7598f91c
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-16 11:21:49 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -28,7 +28,7 @@
 
 
 const notreallycurved =(num, base)=>{
-  const rside = 530
+  const rside = 200
   for(let linesD = 0; linesD!=num; linesD++){
     drawLine(rside/num*linesD, base, 0, base/num*linesD, "black", 0.5)
   }
