8b99e387ab119983a0e889d6e2c275c1d9a92419
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-16 14:43:43 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -23,7 +23,7 @@ const distance = (x, y) =>{
   return Math.abs(x-y);
 }
 const manhattanDistance = (x1, y1, x2, y2) =>{
-  return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+  return distance(x1, x2) + distance(y1, y2);
 }
 const euclideanDistance = (x1, y1, x2, y2) =>{
   return Math.sqrt(Math.abs(x1 - x2)**2 + Math.abs(y1 - y2)**2);
