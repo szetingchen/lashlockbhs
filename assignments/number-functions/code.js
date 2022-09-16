4413a5036a10be903e4d17646b5f3fb5ddc21e92
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-16 14:41:55 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,30 +1,30 @@
-function add(x, y){
-  return x+y
+const add = (x, y) =>{
+  return x+y;
 }
-function subtract(x, y){
-  return x - y
+const subtract = (x, y) =>{
+  return x - y;
 }
-function multiply(x, y){
-  return x * y
+const multiply = (x, y) =>{
+  return x * y;
 }
-function divide(x, y){
-  return x / y
+const divide = (x, y) =>{
+  return x / y;
 }
-function mod(x, y){
-  return x % y
+const mod = (x, y) =>{
+  return x % y;
 }
-function averageOfTwo(x, y){
-  return (x + y)/2
+const averageOfTwo = (x, y) =>{
+  return (x + y)/2;
 }
-function averageOfThree(x, y, z){
-  return (x + y + z)/3
+const averageOfThree = (x, y, z) =>{
+  return (x + y + z)/3;
 }
-function distance(x, y){
-  return Math.abs(x-y)
+const distance = (x, y) =>{
+  return Math.abs(x-y);
 }
-function manhattanDistance(x1, y1, x2, y2){
-  return Math.abs(x1 - x2) + Math.abs(y1 - y2)
+const manhattanDistance = (x1, y1, x2, y2) =>{
+  return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 }
-function euclideanDistance(x1, y1, x2, y2){
-  return Math.sqrt(Math.abs(x1 - x2)**2 + Math.abs(y1 - y2)**2)
+const euclideanDistance = (x1, y1, x2, y2) =>{
+  return Math.sqrt(Math.abs(x1 - x2)**2 + Math.abs(y1 - y2)**2);
 }
