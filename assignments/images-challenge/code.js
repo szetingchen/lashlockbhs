d421a4dba8b5c345a0cc18cef9a6d64591e05d37
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-20 14:43:22 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -37,7 +37,7 @@ const lineOfCircles = (radius) =>{
   const cwidth = radius*2
   const num = Math.floor((width/cwidth))
   for (let i = 0; num>i; i++){
-    drawFilledCircle(cwidth*i, height/2, radius, 'black')
+    drawFilledCircle(radius+cwidth*i, height/2, radius, 'black')
     var numofc = i
   }
 
