ba6af37b9c996392ce92c0007265b82c219c64d9
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-22 15:05:21 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -59,7 +59,7 @@ const concentricCircles = (num, color1, color2) =>{
       drawCircle(width/2, height/2, inc+inc*i, color1, 70)
     }
     else{
-      drawCircle(width/2, height/2, inc+inc*i, color2, 70)
+      drawCircle(width/2, height/2, inc+inc*i, color2)
     }
   }
 }
