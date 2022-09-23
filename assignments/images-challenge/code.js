ae6a6103dbea3e44cf011ce9b34fd6b311325b5d
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-23 15:16:54 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -90,7 +90,7 @@ const fillWithCircles = (r) =>{
   let y = r;
   for (let a = 0; numy>a; a++){
     for (let i = 0; numx>i; i++){
-      drawCircle(r/4 + r + d*i, y-r/2, r, 'black')
+      drawCircle(r/4 + r + d*i, y+r/2, r, 'black')
     }
     y+=d
   }
