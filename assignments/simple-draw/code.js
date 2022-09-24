eb018c5b0e57708f50cfc65c0bdae9ebf48ab728
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 15:12:05 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -42,7 +42,7 @@ function drawTriangle(x, y, size, color) {
  
 }
 */
-
+/*
 var x = -1000
 var y = 350
 var size = 250
@@ -66,4 +66,47 @@ for(let i=0; i < 250; i++){
     var size = 250
     var y = 350
     var x=x+30
+}
+*/
+const z_sqr = (x,y) =>{
+  return [x**2 - y**2, 2*x*y];
+}
+const f = (z) =>{
+  const z_sqr1 = z_sqr(z[0], z[1])
+  return [z_sqr1[0]+z[0], z_sqr1[1]+z[1]];
+}
+const isPixelInSet = (c, iterations) =>{
+  let z = c;
+  for(let i=0; i<iterations; i++){
+    z=f(z);
+    console.log(z)
+    if(z[1]===Infinity||z[0]===Infinity){
+      return false
+    }
+    
+  }
+  return true
+  
+}
+const drawmandel = (iterations) =>{
+  const startx=width/2
+  const starty=height/2
+  let pixelschecked = 0
+  let xmath = 0
+  let ymath = 0
+  let x = 0
+  let y = 0
+  for(let a = 0; a<height; a++){
+    for(let i = 0; i<width; i++){
+      console.log(isPixelInSet([xmath, ymath], iterations))
+      if(isPixelInSet([xmath, ymath], iterations)){
+        drawFilledRect(x, y, 1, 1, 'black')
+      }
+      x++
+      xmath+=2/width
+      pixelschecked++
+    }
+    y++
+    ymath+=2/height
+  }
 }
