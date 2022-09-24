d100de086364fa6f6de329cd6344fd9521eae84f
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 13:58:33 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -150,12 +150,13 @@ const f = (z) =>{
 const isPixelInSet = (c, iterations) =>{
   let z = c;
   for(let i=0; i<iterations; i++){
-  
     z=f(z);
+    console.log(z)
     if(z[1]==="Infinity"||z[0]==="Infinity"){
       break
+      
     }
-    console.log(z)
+    
   }
   return z
 }
