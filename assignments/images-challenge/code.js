44f617e9e39b2fe0580ff0d2b15edcf14cab75a1
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 00:06:48 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -133,7 +133,7 @@ const squareOfCircles = (r, color) =>{
     if(i===26){
       cy=-r
     }
-    drawCircle(offset + r + d*i, -cy, r, color)
+    drawCircle(offset + r + d*i, cy, r, color)
 
   }
 }
