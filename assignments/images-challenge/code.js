f1b7e78882a62db4ad6d9c2d49a3659c3074e067
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 14:05:35 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -170,5 +170,5 @@ const isPixelInSet = (c, iterations) =>{
 //fillWithCircles(30, 'blue')
 //fillWithCirclesRandomlyFilled(30, 0.99, 'blue')
 //squareOfCircles(10, 'blue') //not done 
-const c=[-1, -1]
+const c=[10, -1]
 console.log(isPixelInSet(c, 50))
