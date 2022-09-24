eff3cc6393d238291e603e85b4ffd113947188f3
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 14:11:06 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -161,9 +161,9 @@ const isPixelInSet = (c, iterations) =>{
   
 }
 const drawmandel = (c, iterations) =>{
-
+  
 }
-notreallycurved(20, 530, 500)
+//notreallycurved(20, 530, 500)
 //lineOfCircles(23)
 //lineOfCirclesColor(12, 'pink', 'teal')
 //concentricCircles(11, 'teal', 'pink')
