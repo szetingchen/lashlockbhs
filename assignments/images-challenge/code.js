3c77e87ed4bf0a7330ec9d5d53b618fa4ce66848
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-23 19:38:17 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -117,7 +117,7 @@ const fillWithCirclesRandomlyFilled = (r, prob) =>{
 
 
 //notreallycurved(220, 530, 500)
-lineOfCircles(345)
+lineOfCircles(23)
 //lineOfCirclesColor(20, 'pink', 'teal')
 //concentricCircles(11, 'blue', 'red')
 //checkerBoard(5, 'blue', 'red')
