0d2ea36d2fc22aa14259598ea8e4801b3e216b59
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 22:16:57 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -103,10 +103,10 @@ const drawmandel = (iterations, border) =>{
   const offsetx = 0
   const offsety = 0
   let xmath = 0
-  let ymath = 0  
+  let ymath = 100
   for(let y =0; y<=600+offsety; y++){
     for(let x=0; x<=530+offsetx; x++){
-      if((x<=530+offsetx||y<=600+offsety)&&x-offsetx>=0){
+      if((x<=530+offsetx||y<=600+offsety)&&x-offsetx>=0&&y-offsety>=0){
         xmath=-2+(4/border)*x
         ymath=2-(4/border)*y
 
