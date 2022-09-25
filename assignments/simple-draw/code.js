ebb1c2565e5c3651a7ba922819be7e5494f0cc8e
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 18:04:47 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -74,6 +74,7 @@ function rgb(red, green, blue) {
 }
 
 const z_sqr = (x,y) =>{
+  console.log([x**2 - y**2, 2*x*y])
   return [x**2 - y**2, 2*x*y];
 }
 const f = (z, c) =>{
@@ -124,4 +125,3 @@ const drawmandel = (iterations, border) =>{
 //drawmandel(50, 400)
 //console.log("iterations: "+isPixelInSet([1,1], 50)) 
 console.log(isPixelInSet(0, [5,0], 50))
-console.log(z_sqr(5, 0))
