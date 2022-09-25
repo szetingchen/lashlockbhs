93f6d34b20b111cffaa48512ea1c156bfebe83df
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-25 00:51:11 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -100,7 +100,7 @@ const isPixelInSet = (z, c, iterations) =>{
 
 const drawmandel = (iterations, border) =>{
   const color = 'black'
-  const offsetx = 250
+  const offsetx = 240
   const offsety = (border-600)/2
   let xmath = 0
   let ymath = 0
