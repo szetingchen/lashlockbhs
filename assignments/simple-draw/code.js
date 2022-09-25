bf89086573e8675b0ef356afb77598701e5443b4
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 18:36:50 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -111,11 +111,11 @@ const drawmandel = (iterations, border) =>{
       ymath=2-(4/border)*y
       let pixelinset = isPixelInSet([0,0], [xmath, ymath], iterations)
       //console.log([xmath, ymath] + ": " + pixelinset)
-      if(pixelinset>0){
-        drawFilledRect(x, y, 1, 1, rgb(pixelinset*10,pixelinset*10,pixelinset*10))
+      if(pixelinset===0){
+        drawFilledRect(x, y, 1, 1, "black")
       }
-      else if(pixelinset===0){
-        drawFilledRect(x, y, 1, 1, 'black')
+      else if(pixelinset>0){
+        drawFilledRect(x, y, 1, 1, rgb(pixelinset*10,pixelinset*10,pixelinset*10))
       }
 
       pixelschecked++
