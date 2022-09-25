9518f04fa663457e54800f847dcdf497d2e5b926
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 21:37:29 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -116,7 +116,7 @@ const drawmandel = (iterations, border, startx, starty) =>{
         }
         else if(pixelinset>0){
         
-          drawLine(x-100, y, x+1-100, y, rgb(6*pixelinset, 40*pixelinset, 200*pixelinset))
+          drawLine(x-200, y, x+1-200, y, rgb(6*pixelinset, 40*pixelinset, 200*pixelinset))
         }
 
         pixelschecked++
