370b0a5bb15db6631d9345c0aebfbe88b31ca121
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-24 23:39:13 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -123,6 +123,9 @@ const drawmandel = (iterations, border) =>{
     }
   }
 }
-drawmandel(50, 600)
+drawmandel(50, 1200)
 //console.log("iterations: "+isPixelInSet([1,1], 50)) 
 //console.log(isPixelInSet([0,0], [5,0], 50))
+
+
+//-1.6466666666666667, -2
