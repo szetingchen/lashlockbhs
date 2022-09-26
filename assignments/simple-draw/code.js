8728c82beed61155287bd21132a4405e199dd8f7
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-25 20:04:45 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -102,8 +102,6 @@ const drawmandel = (iterations, border, zoomx, zoomy) =>{
   const color = 'black'
   const xbasedoz = border*(zoomx/600)
   const ybasedoz = border*(zoomy/600)
-  console.log(xbasedoz)
-  console.log(ybasedoz)
   let offsetx;
   let offsety;
   if(border===600){
