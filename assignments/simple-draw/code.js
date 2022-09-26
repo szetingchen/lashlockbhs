b78be7859d4f955649e580580cf502c86c198868
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-26 15:01:45 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -102,8 +102,8 @@ const drawmandel = (iterations, borderx, bordery, centerx, centery, zoom) =>{
   let count = 0;
   drawLine(width/2, 0, width/2, height, 'black')
   drawLine(0, height/2, width, height/2, 'black')
-  for(let y = 0; y<=border; y++){
-    for(let x= 0; x<=border; x++){
+  for(let y = 0; y<=bordery; y++){
+    for(let x= 0; x<=borderx; x++){
       
       xmath=-2+((4/2)/border)*x
       ymath=2-((4/2)/border)*y
