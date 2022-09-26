d34aa494c5c70d67d931c280dd991aaa03bb1f9c
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-25 21:02:04 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -145,7 +145,7 @@ const drawmandel = (iterations, border, zoomx, zoomy) =>{
 
 
 const x = -64
-const y = -28
+const y = -25
 drawmandel(40, 600, x+width/2, y+height/2)
 //console.log("iterations: "+isPixelInSet([1,1], 50)) 
 //console.log(isPixelInSet([0,0], [5,0], 50))
