70a229a5db969011922eb1684d80399c9f242ec0
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-26 12:22:49 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -222,6 +222,6 @@ const drawmandel = (iterations, border, zoomx, zoomy) =>{
 //fillWithCirclesRandomlyFilled(30, 0.99, 'blue')
 //squareOfCircles(10, 'blue') //not done 
 const x = 0
-const y = 50
+const y = 100
 drawmandel(10000, 600, x+width/2, y+height/2)
 
