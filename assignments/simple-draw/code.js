d87cb25a2e1f088b27b4efb11f922b581b3f7b50
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-25 19:24:40 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -101,7 +101,7 @@ const isPixelInSet = (z, c, iterations) =>{
 const drawmandel = (iterations, border, zoomx, zoomy) =>{
   const color = 'black'
   const offsetx = ((zoomx/border)*border)-(border*(border-600))
-  const offsety = ((zoomy/border)*border)-border
+  const offsety = ((zoomy/border)*border)-(border*(border-600))
   let xmath = 0
   let ymath = 0
   drawLine(width/2, 0, width/2, height, 'black')
