91424b28bedc119086d101f44f6626f671d26d57
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-26 12:19:48 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -26,6 +26,7 @@
  *
  */
 
+/*
 
 const notreallycurved =(num, base, rside)=>{
   for(let linesD = 0; linesD!=num; linesD++){
@@ -139,6 +140,7 @@ const squareOfCircles = (r, color) =>{
     //not done
   }
 }
+ */
 
 //madnelstart
 function rgb(red, green, blue) {
