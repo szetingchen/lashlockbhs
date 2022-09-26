9940aeb0857309b9f689407293424082e02808d2
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-25 22:40:03 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -127,7 +127,7 @@ const drawmandel = (iterations, border, zoomx, zoomy) =>{
           count++
         }
         else if(pixelinset>0){
-          drawLine(x-offsetx, y-offsety, x+1-offsetx, y-offsety, rgb(pixelinset*1+(iterations/iterations*100), pixelinset*1+(iterations/iterations*100),pixelinset*1+(iterations/iterations*100)))
+          drawLine(x-offsetx, y-offsety, x+1-offsetx, y-offsety, rgb(pixelinset*1+(iterations/iterations*1.5), pixelinset*1+(iterations/iterations*1.5),pixelinset*1+(iterations/iterations*1.5)))
           count++
         }
       }
