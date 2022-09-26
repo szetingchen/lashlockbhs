43880c1b518d30317d2c263fe18698f71318268e
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-25 19:53:35 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -111,8 +111,8 @@ const drawmandel = (iterations, border, zoomx, zoomy) =>{
     offsety=0;
   }
   else{
-    offsetx = 600-xbasedoz;
-    offsety = 600-ybasedoz;
+    offsetx = border-xbasedoz;
+    offsety = border-ybasedoz;
   }
   console.log(offsetx)
   console.log(offsety)
