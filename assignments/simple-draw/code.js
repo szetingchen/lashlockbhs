6545f3120ea40440a71272413dfb30572d8a6487
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-28 14:42:27 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -137,8 +137,8 @@ const drawmandel = (iterations, borderx, bordery, centerx, centery, zoom) =>{
 //let y = -0.64935990748190
 //let x = 165
 //let y = 0
-const x = -100
-const y = 100
+const x = -50
+const y = 50
 drawmandel(10000, height, height, x, y, 1)
 
 
