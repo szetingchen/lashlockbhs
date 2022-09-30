d07b71de5674101a5a30175eab78acbe7b33b67f
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-30 15:07:48 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -39,5 +39,5 @@ const getsSpeedingTicket = (mph, isGrouchy) => {
 }
 
 const moreThanTwiceAsLong = (str1, str2) => {
-  return (str1.length()*2) > str2.length();
+  return (str1.length*2) > str2.length;
 }
