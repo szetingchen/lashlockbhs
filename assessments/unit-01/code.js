161e5c971672722656dc8fbb6c4f1f7b9d0294ca
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-30 15:20:52 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -61,7 +61,7 @@ const upDown = (str) => {
 
 const everyOther = (str) => {
   let str1;
-  for(let i = 0; i<str.length()/2; i+=2){
+  for(let i = 0; i<str.length/2; i+=2){
     str1+=str1.substring(i);
   }
   return str1;
