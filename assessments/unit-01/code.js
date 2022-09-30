9dedd2949b1f16fb2948ccffbd10f0abbb9e5d92
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-09-30 14:37:31 -0700

    Creating assessments/unit-01/code.js

---
+++ assessments/unit-01/code.js
@@ -0,0 +1,7 @@
+// You will use this function in one of the problems. You don't need to change
+// it or even worry about how exactly it works.
+const emit = (a, b, product) => {
+  console.log(`${a} times ${b} is ${product}`);
+};
+
+// Write your code here ...
