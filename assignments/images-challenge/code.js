5e28d2bc34f62411dea324430d518330297b3faa
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-03 15:13:26 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -68,11 +68,12 @@ const concentricCircles = (num, color1, color2) => {
 }
 const checkerBoard = (num, color1, color2) => {
   const smallerD = width > height ? height : width
-  const size = smallerD / num
+  const size = smallerD / num 
   let color11 = color1;
   let color22 = color2;
-  for (let y = 0; y < num; y-=size) {
+  for (let y = 0; y < num; y+=size) {
     for (let x = 0; x < num; x+=size) {
+      console.log(x+ y)
       if (x % 2 === 0) {
         drawFilledRect(x, y, size, size, color11)
       }
