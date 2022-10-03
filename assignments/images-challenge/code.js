77e81635baae883aa7727b3500826d7db086e924
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-03 15:05:39 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -28,114 +28,115 @@
 
 
 
-const notreallycurved =(num, base, rside)=>{
-  for(let linesD = 0; linesD!=num; linesD++){
-    drawLine(rside/num*linesD, base, 0, base/num*linesD, "black", 0.5)
+const notreallycurved = (num, base, rside) => {
+  for (let linesD = 0; linesD != num; linesD++) {
+    drawLine(rside / num * linesD, base, 0, base / num * linesD, "black", 0.5)
   }
 }
-const lineOfCircles = (r) =>{
-  const d = r*2
-  const num = Math.floor(width/d)
-  const offset = (width-num*d)/2
-  for (let i = 0; num>i; i++){
-    drawFilledCircle(offset + r + d*i, height/2, r, 'black')
+const lineOfCircles = (r) => {
+  const d = r * 2
+  const num = Math.floor(width / d)
+  const offset = (width - num * d) / 2
+  for (let i = 0; i < num; i++) {
+    drawFilledCircle(offset + r + d * i, height / 2, r, 'black')
   }
 }
-const lineOfCirclesColor = (r, color1, color2) =>{
-  const d = r*2
-  const num = Math.floor(width/d)
-  const offset = (width-num*d)/2
-  for (let i = 0; num>i; i++){
-    if(i%2===0){
-      drawFilledCircle(offset + r + d*i, height/2, r, color1)
+const lineOfCirclesColor = (r, color1, color2) => {
+  const d = r * 2
+  const num = Math.floor(width / d)
+  const offset = (width - num * d) / 2
+  for (let i = 0; i < num; i++) {
+    if (i % 2 === 0) {
+      drawFilledCircle(offset + r + d * i, height / 2, r, color1)
     }
-    else{
-      drawFilledCircle(offset + r + d*i, height/2, r, color2)
-    }
-  }
-}
-const concentricCircles = (num, color1, color2) =>{
-  const smallerD = width>height ? height : width
-  const inc = smallerD/num/2
-  for (let i = 0; num>i; i++){
-    if(i%2===0){
-      drawFilledCircle(width/2, height/2, smallerD/2-inc*i, color1)
-    }
-    else{
-      drawFilledCircle(width/2, height/2, smallerD/2-inc*i, color2)
+    else {
+      drawFilledCircle(offset + r + d * i, height / 2, r, color2)
     }
   }
 }
-const checkerBoard = (num, color1, color2) =>{
-  const size = width/num
+const concentricCircles = (num, color1, color2) => {
+  const smallerD = width > height ? height : width
+  const inc = smallerD / num / 2
+  for (let i = 0; i < num; i++) {
+    if (i % 2 === 0) {
+      drawFilledCircle(width / 2, height / 2, smallerD / 2 - inc * i, color1)
+    }
+    else {
+      drawFilledCircle(width / 2, height / 2, smallerD / 2 - inc * i, color2)
+    }
+  }
+}
+const checkerBoard = (num, color1, color2) => {
+  const smallerD = width > height ? height : width
+  const size = height / num
   let color11 = color1;
-  let color22 = color2; 
+  let color22 = color2;
   let y = 50;
-  for (let a = 0; num>a; a++){
-    for (let i = 0; num>i; i++){
-      if(i%2===0){
-        drawFilledRect(size*i, y, size, size, color11)
+  for (let a = 0; a < num; a++) {
+    for (let i = 0; i < num; i++) {
+      if (i % 2 === 0) {
+        drawFilledRect(size * i, y, size, size, color11)
       }
-      else{
-        drawFilledRect(size*i, y, size, size, color22)
+      else {
+        drawFilledRect(size * i, y, size, size, color22)
       }
     }
-    y+=size
+    y += size
     let temp = color11;
-    color11 = color22; 
-    color22 = temp 
+    color11 = color22;
+    color22 = temp
   }
 }
-const fillWithCircles = (r, color) =>{
-  const d = r*2
-  const numx = Math.floor(width/d)
-  const numy = Math.floor(height/d)
-  const offsetx = (width-(numx*d))/2
-  const offsety = (height-(numy*d))/2
-  for (let y = 0; numy>y; y+=d){
-    for (let x = 0; numx>x; x+=d){
-      drawCircle(offsetx+x, offsety+y, r, color)
+const fillWithCircles = (r, color) => {
+  const d = r * 2
+  const numx = Math.floor(width / d)
+  const numy = Math.floor(height / d)
+  const offsetx = (width - (numx * d)) / 2
+  const offsety = (height - (numy * d)) / 2
+  for (let y = 0; y < numy; y += d) {
+    for (let x = 0; x < numx; x += d) {
+      drawCircle(offsetx + x, offsety + y, r, color)
     }
-    y+=d
+    y += d
   }
 }
-const fillWithCirclesRandomlyFilled = (r, prob, color) =>{
-  const d = r*2
-  const numx = Math.floor(width/d)
-  const numy = Math.floor(height/d)
-  const offsetx = (width-(numx*d))/2
-  const offsety = (height-(numy*d))/2
+const fillWithCirclesRandomlyFilled = (r, prob, color) => {
+  const d = r * 2
+  const numx = Math.floor(width / d)
+  const numy = Math.floor(height / d)
+  const offsetx = (width - (numx * d)) / 2
+  const offsety = (height - (numy * d)) / 2
   let numfilled = 0;
   let y = r;
-  for (let a = 0; numy>a; a++){
-    for (let i = 0; numx>i; i++){
-      if (Math.random()<=prob){
-        drawFilledCircle(offsetx + r + d*i, offsety + y, r, color)
+  for (let a = 0; a < numy; a++) {
+    for (let i = 0; i < numx; i++) {
+      if (Math.random() <= prob) {
+        drawFilledCircle(offsetx + r + d * i, offsety + y, r, color)
         numfilled++
       }
-      else{ 
-        drawCircle(offsetx + r + d*i, offsety + y, r, color)
-        
+      else {
+        drawCircle(offsetx + r + d * i, offsety + y, r, color)
+
       }
     }
-    y+=d
+    y += d
   }
-  console.log("num circles:"+numx*numy)
-  console.log("num filled:"+numfilled)
+  console.log("num circles:" + numx * numy)
+  console.log("num filled:" + numfilled)
 }
 
-const squareOfCircles = (radius, color) =>{
-  const diameter = 2*radius
-  const cNum = Math.floor(height/diameter)
-  const cExtra = (height - (diameter*cNum))/2+radius
+const squareOfCircles = (radius, color) => {
+  const diameter = 2 * radius
+  const cNum = Math.floor(height / diameter)
+  const cExtra = (height - (diameter * cNum)) / 2 + radius
   let xCheese;
-  for(let xPos = (width - height+cExtra)/2;xPos < width - ((width - height)- cExtra)/2-radius; xPos += diameter){
+  for (let xPos = (width - height + cExtra) / 2; xPos < width - ((width - height) - cExtra) / 2 - radius; xPos += diameter) {
     drawCircle(xPos, height - cExtra, radius, color)
-    drawCircle (xPos, cExtra, radius, color)
+    drawCircle(xPos, cExtra, radius, color)
     xCheese = xPos
   }
-  for(let yPos = cExtra+diameter; yPos < height-cExtra; yPos += diameter){
-    drawCircle((width-height+cExtra)/2,yPos, radius, color)
+  for (let yPos = cExtra + diameter; yPos < height - cExtra; yPos += diameter) {
+    drawCircle((width - height + cExtra) / 2, yPos, radius, color)
     drawCircle(xCheese, yPos, radius, color)
   }
 }
