90d3c5b5b159eedce9d5a9926ac219e3930e7a2e
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-06 15:24:41 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -71,7 +71,7 @@ const checkerBoard = (num, color1, color2) => {
   const size = smallerD / num;
   let color=color2;
   let switchx = false;
-  for (let numSquaresy = 0; numSquaresy<num; numSquaresy+=2) {
+  for (let numSquaresy = 0; numSquaresy<num*2; numSquaresy+=2) {
     for (let numSquaresx = 0; numSquaresx<num/2; numSquaresx++) {
       drawFilledRect(numSquaresx*size, switchx ? size : 0 + numSquaresy*size, size, size, color);
       console.log(' ')
