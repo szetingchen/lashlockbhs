42142ad1ed5c162becdb0173c1a7e20ed34cf5ee
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-06 14:36:10 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -84,7 +84,7 @@ const checkerBoard = (num, color1, color2) => {
     color22 = temp
   }
 }
-checkerBoard(11, 'white', 'black')
+checkerBoard(10, 'white', 'black')
 
 
 const fillWithCircles = (r, color) => {
