ef0322964d3b43c30d56f8ba87d0ed9985cb0625
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-07 14:35:42 -0700

    Creating assignments/chess-board/code.js

---
+++ assignments/chess-board/code.js
@@ -0,0 +1,15 @@
+const WHITE_KING   = '♔';
+const WHITE_QUEEN  = '♕';
+const WHITE_ROOK   = '♖';
+const WHITE_BISHOP = '♗';
+const WHITE_KNIGHT = '♘';
+const WHITE_PAWN   = '♙';
+const BLACK_KING   = '♚';
+const BLACK_QUEEN  = '♛';
+const BLACK_ROOK   = '♜';
+const BLACK_BISHOP = '♝';
+const BLACK_KNIGHT = '♞';
+const BLACK_PAWN   = '♟';
+
+// Example of drawing one of the pieces
+drawText(WHITE_KING, width/2, height/2, 'black', 64);
