dc4bbc3f2946b25876e3c582b9dc1ee7a9816442
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-14 14:41:24 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -19,7 +19,7 @@ const sum = (array) => {
 const evens = (array) => {
   let arraydos=[]
   for (let i = 0; i < array.length; i++) {
-    if (array[i]%2===10){
+    if (array[i]%2===0){
       arraydos.push(array[i])
     }
   }
