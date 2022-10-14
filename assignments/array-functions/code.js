184a23592b2ca8c3c0fd48603a7476bb5770e73d
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-14 14:38:50 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,9 +1,17 @@
-const countTens = (array) =>{
-  let num=0;
-  for(let i =0; i<array.length; i++){
-    if(array[i]===10){
+const countTens = (array) => {
+  let num = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 10) {
       num++;
     }
+  }
+  return num;
+}
+
+const sum = (array) => {
+  let num = 0;
+  for (let i = 0; i < array.length; i++) {
+    num+=array[i]
   }
   return num;
 }
