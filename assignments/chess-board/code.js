779be8433baee5fdc4dfb29c52381de5b083a23f
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-19 15:21:58 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -79,6 +79,7 @@ const canBePlaced = (ay, ax, sudostart, placement) => {
     for (let y = 0; y < 9; y++) {
 
       console.log(x + ", " + y)
+      console.log(ax + ", " + ay)
       console.log("target box: " + sudostarttest[ax][ay][1] + " check box: " + sudostarttest[x][y][1])
       console.log("target num: " + sudostarttest[ax][ay][0] + " check num: " + sudostarttest[x][y][0])
 
