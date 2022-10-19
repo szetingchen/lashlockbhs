26e6a09e7de218d7bcc0daaf918e58cbb8c071c6
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-19 15:15:50 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -82,7 +82,7 @@ const canBePlaced = (ay, ax, sudostart, placement) => {
       console.log("target box: " + sudostarttest[ax][ay][1] + " check box: " + sudostarttest[x][y][1])
       console.log("target num: " + sudostarttest[ax][ay][0] + " check num: " + sudostarttest[x][y][0])
 
-      if ((sudostarttest[x][y][1] === sudostarttest[ax][ay][1]) && (sudostarttest[x][y][0] === sudostarttest[ax][ay][0])) {
+      if ((sudostarttest[x][y][1] === sudostarttest[ax][ay][1]) && (sudostarttest[x][y][0] === sudostarttest[ax][ay][0]) && x != ax && y != ay) {
         inBox = true;
 
         console.log("in box")
