bdaa343a275b2848fa51b3fcb472225fcf4f8f75
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-18 21:06:34 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -154,8 +154,8 @@ const drawSudo = (array) => {
   }
   for (let y = 0; y<9; y++){
     for (let x = 0; x<9; x++){
-      console.log(array[x][y][0])
-      drawText(array[x][y][0], (x*spacing), (y*spacing)+100, 'black', spacing)
+      console.log(array[y][x][0])
+      drawText(array[y][x][0], (x*spacing), (y*spacing)+100, 'black', spacing)
     }
   }
 }
