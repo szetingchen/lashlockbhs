593ee127e0372efad0cc7436445a2b23c690f31f
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-19 21:17:15 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -74,8 +74,8 @@ const canBePlaced = (ay, ax, sudostart, placement) => {
   let inVert = false;
   let inHort = false;
   let inBox = false;
-  for (let x = 0; x < 9; x++) {
-    for (let y = 0; y < 9; y++) {
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
       console.log(x + ", " + y)
       if ((sudostarttest[x][y][1] === sudostarttest[ax][ay][1]) && (sudostarttest[x][y][0] === sudostarttest[ax][ay][0]) && !((ax === x) && (ay === y))) {
         inBox = true;
