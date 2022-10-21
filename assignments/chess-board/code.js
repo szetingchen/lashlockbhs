32c002c39d4da1c57f98f2c5818ee58ea198ce37
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-20 19:16:34 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -144,8 +144,8 @@ const solve_aspossible = (array) => {
       if(possible.length===1){
         newarray[x][y][0]=possible[0]
         console.log("changed: " + "("+ possible[0]+")" + "newarray point check: " + newarray[x][y][0])
+        console.log(newarray)
         drawSudo(newarray)
-
       }
     }
   }
