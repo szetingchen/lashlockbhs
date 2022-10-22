b0f2fca1224655091c955d8d5f320e6e6c515d24
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-21 23:45:18 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -96,7 +96,7 @@ const convert = (mode, x, y, array) => {
     }
 
     for (let i = 0; i < 9; i++) {
-      newAr.push(convert2Box[y])
+      newAr.push(convert2Box[i])
     }
     console.log(newAr)
   }
