b2851bab22c4169bf14bd446778605aef7f54851
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-24 14:54:06 -0700

    Creating assignments/tic-tac-toe/code.js

---
+++ assignments/tic-tac-toe/code.js
@@ -0,0 +1,10 @@
+// This is a bit of a new thing. registerOnclick is a function provided by the
+// framework. But the argument we're passing to it is *another* function. Notice
+// how the argument here looks like what we normally put on the righthand side
+// of the equals sign in our normal `const foo = ...` function definition. This
+// is called an anonymous function. We'll discuss this in more detail in a few
+// weeks but for now you can just adapt this code.
+
+registerOnclick((x, y) => {
+  drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
+});
