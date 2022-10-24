cbc3e190325252dce276fd0f85fb95a3e9fa43b4
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-24 15:32:09 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -134,7 +134,7 @@ const printSudo = (array) => {
   for (let y = 0; y < 9; y++) {
     for (let x = 0; x < 9; x++) {
       if(y+1%3===0){
-        console.log(line)
+        str+=line
       }
       if((x)%3===0){
         str += "|"
