6899f4775629b79cf0acf3a8bf2e2c6621361971
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-24 20:34:45 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -74,7 +74,7 @@ const canBePlaced = (ay, ax, sudostart1, placement) => {
     }
   }
   for (let i = 0; i < 9; i++) {
-    console.log(ay + ", "  + i)
+    console.log(i + ", "  + ay)
     if (sudostart1[ay][i]===placement) {
       return false
     }
