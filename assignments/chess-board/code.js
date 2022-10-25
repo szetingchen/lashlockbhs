b19a49557a0a95c81360d4a582b9b2baa8ce569a
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-24 19:59:03 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -187,6 +187,7 @@ const solve = (array) => {
   for (let i = 1; i <= 9; i++) {
     printSudo(array)
     console.log(x + ", " + y + " : " + i + " : " + canBePlaced(x, y, array, i))
+    console.log("_____________________")
     if (canBePlaced(x, y, array, i)) {
       console.log("placed")
       array[y][x][0] = i
