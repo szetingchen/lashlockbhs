25ed2f854b33e708ff9b8e512d640c1b4ce0faa6
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-25 15:16:15 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -8,7 +8,7 @@
 
 const drawTik = () => {
   const spacing = width/10
-  drawLine(width/2*2/3, height*2/3, width/2*2/3, height*1/6, 'black', 1)
+  drawLine(width/2*2/3, height*2/6, width/2*2/3, height*1/6, 'black', 1)
   
 }
 
