d9ea4ce7da66028dbf93b31d15e7b666ac21764c
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-26 14:57:55 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -23,6 +23,7 @@ registerOnclick((x, y) => {
   console.log(drawTik("e"))
   const offset = 0
   const box_x = Math.floor((x-width/2-drawTik("e")*1.5)/drawTik("e"))
+  console.log(box_x)
   const box_y = y
   const turn = "x"
   if(turn==="x"){
