0c0227adb8f18aeddd897bd3f9b0aa060f1b77ae
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-26 15:02:49 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -21,7 +21,7 @@ const drawTik = (spacingIn) => {
 drawTik()
 registerOnclick((x, y) => { 
   console.log(drawTik("e"))
-  console.log(x+width/2-drawTik("e")*1.5)
+  console.log(x)
   const offset = 0
   const box_x = Math.floor((x-width/2-drawTik("e")*1.5)/drawTik("e"))
   console.log(box_x)
