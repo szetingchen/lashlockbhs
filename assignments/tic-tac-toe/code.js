61b6769033b3c89ecf2c4cae1a46a9bc03f48358
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-26 20:03:28 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -31,20 +31,20 @@ const drawTik = (board, spacingIn) => {
 
 const canKeepPlaying = (board) => {
   for (let i = 0; i < 3; i++) {
-    if (board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+    if (board[0][i] === board[1][i] && board[0][i] === board[2][i] && board[0][i] === "O" || "X") {
       return false;
     }
   }
   for (let i = 0; i < 3; i++) {
     {
-      if (board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+      if (board[i][0] === board[i][1] && board[i][0] === board[i][2] && board[i][0] === "O" || "X") {
         return false;
       }
     }
-    if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+    if (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] === "O" || "X") {
       return false;
     }
-    if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+    if (board[0][2] === board[1][1] && board[0][2] === board[2][0] && board[0][2] === "O" || "X") {
       return false;
     }
     return true;
