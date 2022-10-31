92f7dbb520ee09aff09e4a4a0850e603e62e03f5
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-10-31 15:29:58 -0700

    Updating assignments/game-of-life/code.js

---
+++ assignments/game-of-life/code.js
@@ -0,0 +1,23 @@
+
+
+//if the sum of all nine fields in a given neighbourhood is three, 
+//the inner field state for the next generation will be life; if the all-field sum is four, 
+//the inner field retains its current state; and every other sum sets the inner field to death.
+
+
+
+//idea
+//create 2d array that stores data for all cells, size of array based off how many cells 
+//can be fit on screen
+//size of cells will be changeable
+
+//convert 2d array pos to screen pos: (width/sizeOfCell*arX), (height/sizeOfCell*arY)
+
+//need to get all neighbors of a certain cell, maybe func?
+
+//change state to 0 if dead or 1 if alive
+
+//animate each state
+
+//speed var?
+
