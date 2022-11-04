b6694cca554ac45d8202307199efeb372a27d719
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-11-04 15:05:02 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -22,8 +22,8 @@ const startBoard = [
   [BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_KING, BLACK_QUEEN, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK],
 ]
 const drawBoard = (board) => {
-  const heightTest = 200
-  const widthTest = 400
+  const heightTest = hieght
+  const widthTest = width
   const size = (widthTest/3)/8
   for (let yAr = 0; yAr < 8; yAr++) {
     for (let xAr = 0; xAr < board[yAr].length; xAr++) {
