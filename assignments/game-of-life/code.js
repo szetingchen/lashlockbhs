903dce2e2b6621c1320d70592700e364ee4c501f
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-11-07 08:13:00 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -23,7 +23,7 @@
 
 
 const pixSizeFactor = 60
-const timeTillUpdate = 100;
+const timeTillUpdate = 10;
 
 const declareAr = (pixSizeFactor) => {
   const array = []
@@ -100,7 +100,6 @@ const drawFrame = (time) => {
   clear();
   if(time%timeTillUpdate===0){
     calcTimeBasedGen(screenAr, time, timeTillUpdate)
-    
   }
   
 }
