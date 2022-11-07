6223ca7fa7a0c82a3dc825f80447cfda16ffb324
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-11-07 12:19:53 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -9,3 +9,7 @@ const point = (x, y) =>{
 const emptyObject = () =>{
   return {}
 }
+
+const distance = (point1, point2) =>{
+  return Math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+}
