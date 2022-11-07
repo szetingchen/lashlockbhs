c54c52e2f72b938f9100fcaabf6de7b41590fc35
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-11-07 15:32:32 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -21,6 +21,7 @@ const midpoint = (point1, point2) =>{
 const sumSalaries = (array) => {
   let sum = 0;
   for(let i = 0; i<array.length; i++){
-    sum+=array[i]
+    sum+=array[i][0]
   }
+  return sum
 }
