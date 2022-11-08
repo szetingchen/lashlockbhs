a0bfa0eb3c0f75bba2c62da975b9e26d7bf085cf
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-11-08 14:33:02 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -15,7 +15,7 @@ const distance = (point1, point2) =>{
 }
 
 const midpoint = (point1, point2) =>{
-  return {"x" : point1.x+(point2.x-point1.x)/2, "y" : point1.y+(point2.y-point1.y)/2}
+  return {"x" : (point2.x+point1.x)/2, "y" : (point2.y+point1.y)/2}
 }
 
 const sumSalaries = (array) => {
