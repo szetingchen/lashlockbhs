5efa400484ff35777bddb033a13994ddc0ef9841
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-11-14 14:56:54 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -2,15 +2,19 @@
 // should save you some time.
 
 const area = (rect) => {
+  return rect.width * rect.height;
 };
 
 const higherPaid = (e1, e2) => {
+  return e1.salary > e2.salary ? e1 : e2;
 };
 
 const isSamePoint = (p1, p2) => {
+  return (p1.x === p2.x) && (p1.y === p2.y);
 };
 
 const totalWithTip = (bill, tipPercentage) => {
+  return {"subtotal" : bill.subtotal, "tip" : bill.subtotal*tipPercentage, "total" : bill.subtotal+bill.subtotal*tipPercentage}
 };
 
 const isWinner = (player) => {
