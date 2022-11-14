52072f837eb0f86b12c82f492db7cf57ef4c93ff
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-11-14 14:49:42 -0800

    Creating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

---
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -0,0 +1,32 @@
+// For a change of pace, I'm providing you with all the function skeletons. This
+// should save you some time.
+
+const area = (rect) => {
+};
+
+const higherPaid = (e1, e2) => {
+};
+
+const isSamePoint = (p1, p2) => {
+};
+
+const totalWithTip = (bill, tipPercentage) => {
+};
+
+const isWinner = (player) => {
+};
+
+const updateWins = (players) => {
+};
+
+const bigWinners = (players) => {
+};
+
+const fillTimesTable = (table) => {
+};
+
+const sums = (n) => {
+};
+
+const rule110 = (cells) => {
+};
