229adeee9e286389daf3951ad89d149c20b657a4
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-11-15 15:02:14 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -1,54 +1,15 @@
-const WHITE_KING = '♔';
-const WHITE_QUEEN = '♕';
-const WHITE_ROOK = '♖';
+const WHITE_KING   = '♔';
+const WHITE_QUEEN  = '♕';
+const WHITE_ROOK   = '♖';
 const WHITE_BISHOP = '♗';
 const WHITE_KNIGHT = '♘';
-const WHITE_PAWN = '♙';
-const BLACK_KING = '♚';
-const BLACK_QUEEN = '♛';
-const BLACK_ROOK = '♜';
+const WHITE_PAWN   = '♙';
+const BLACK_KING   = '♚';
+const BLACK_QUEEN  = '♛';
+const BLACK_ROOK   = '♜';
 const BLACK_BISHOP = '♝';
 const BLACK_KNIGHT = '♞';
-const BLACK_PAWN = '♟';
+const BLACK_PAWN   = '♟';
 
-const startBoard = [
-  [WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_KING, WHITE_QUEEN, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK],
-  [WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN],
-  [],
-  [],
-  [],
-  [],
-  [BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN],
-  [BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_KING, BLACK_QUEEN, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK],
-]
-const drawBoard = (board) => {
-  const smallerD = height < width ? height : width
-  const size = smallerD/8
-  for (let yAr = 0; yAr < 8; yAr++) {
-    for (let xAr = 0; xAr < board[yAr].length; xAr++) {
-      if (board[yAr][xAr] != undefined) {
-        drawText(board[yAr][xAr], size * xAr, (smallerD)/(smallerD/size)+size * yAr, 'black', size);
-      }
-    }
-  }
-}
-
-
-drawBoard(startBoard)
-/*
-const checkMove = (board, piecex, piecey, targetx, targety, mode) => {
-  drawBoard(board)
-  const piece = board[piecex][piecey]
-  const targetEmpty = board[targetx][targety] === undefined
-  const targetFriendly = board[targetx][targety]
-  if (piece === BLACK_PAWN || WHITE_PAWN) {
-    if (piecex - targetx === 1 || piecey - targety === 1){
-      
-    }
-  }
-}
-checkMove(startBoard, 0, 1, 6, 0, null)
-*/
-
-
-
+// Example of drawing one of the pieces
+drawText(WHITE_KING, width/2, height/2, 'black', 64);
