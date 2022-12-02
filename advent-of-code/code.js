c25f5de88c8a9c019768a6ede9067a62d4e55368
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-02 15:10:01 -0800

    Creating advent-of-code/code.js

---
+++ advent-of-code/code.js
@@ -0,0 +1,14 @@
+// Write your code here. To run a function on a given test file you need to
+// create a file in your github repo in the advent-of-code branch and in the
+// advent-of-code directory. Then you can use a function `run` to run a
+// particular function on the contents of the file, which will be passed to your
+// function as a string.
+//
+// For example, if I've created a file 'day_01.test' to contain the test data
+// from day 1, and a function, day01Part1, I can run the function with this
+// call:
+//
+//   run('day_01.test', day01Part1)
+//
+// Which will load the file and pass them to your function and then print the
+// return value in the REPL.
