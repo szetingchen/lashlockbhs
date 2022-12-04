8610efac01e3a1698c8af117dc19ca59791ea516
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-04 13:36:58 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -105,7 +105,7 @@ const day02Part2 = (str) =>{
     let goal = games[i].substring(2, 3)
 
     let thereplay = games[i].substring(0, 1)
-    let indexofourplay = them.indexOf(thereplay) - (goals.indexOf(goal)-1)
+    let indexofourplay = them.indexOf(thereplay) + (goals.indexOf(goal)-1)
     console.log("indexofourplay math: "  + them.indexOf(thereplay) + " - " + (goals.indexOf(goal)-1))
     let ourplay = indexofourplay === -1 ? us[2] : us[indexofourplay]
 
