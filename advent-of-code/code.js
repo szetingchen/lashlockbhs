6f1da48f808b08b217db49080c47fc0f04c09abd
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-03 22:01:59 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -60,15 +60,15 @@ const day02Part1 = (str) => {
     console.log(ourplay)
     if (us[them.indexOf(thereplay) - 1] === ourplay) {
       score += 6 + us.indexOf(ourplay)+1
-      console.log("WIN: 6 + " + (us.indexOf(ourplay)+1))
+      console.log("WIN: 6 + " + (us.indexOf(ourplay)+1) + " " + ourplay + " " + thereplay)
     }
     else if (them.indexOf(thereplay) === us.indexOf(ourplay)) {
       score += 3 + us.indexOf(ourplay)+1
-      console.log("TIE: 3 + " + (us.indexOf(ourplay)+1))
+      console.log("TIE: 3 + " + (us.indexOf(ourplay)+1) + " " + ourplay + " " + thereplay)
     }
     else {
       score += us.indexOf(ourplay)+1
-      console.log("LOSS: " + (us.indexOf(ourplay)+1))
+      console.log("LOSS: " + (us.indexOf(ourplay)+1) + " " + ourplay + " " + thereplay)
     }
   }
   return score
