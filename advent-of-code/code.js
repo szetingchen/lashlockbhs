6fcadb8e6ba41dff613012046f650dce7f3aa656
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-03 21:47:17 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -14,22 +14,62 @@
 // return value in the REPL.
 
 
-const day01Part1 = (str) =>{
+const day01Part1 = (str) => {
   const numbers = str.split('\n');
-  let currelfnum = 0
-  let max = 0
-  for(let i = 0; i<numbers.length; i++){
-    if(numbers[i]!=""){
-      currelfnum+=parseInt(numbers[i])
+  let currelfnum = 0;
+  let max = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] != "") {
+      currelfnum += parseInt(numbers[i]);
     }
-    else{
-      if(currelfnum > max){
-        max = currelfnum
+    else {
+      if (currelfnum > max) {
+        max = currelfnum;
       }
-      currelfnum = 0
+      currelfnum = 0;
     }
   }
   return max;
+}
+
+const day02Part1 = (str) => {
+  const games = str.split("\n");
+  let score = 0;
+  const them = ["A", "B", "C"]
+  const us = ["X", "Y", "Z"]
+  for (let i = 0; i < games.length; i++) {
+    let thereplay = games[i].substring(0, 1)
+    let ourplay = games[i].substring(2, 3)
+    if (us[them.findIndex(thereplay) - 1] === ourplay) {
+      score += 6 + us.findIndex(ourplay)
+    }
+    else if (them.findIndex(thereplay) === us.findIndex(ourplay)) {
+      score += 3 + us.findIndex(ourplay)
+    }
+    else {
+      score += us.findIndex(ourplay)
+    }
+
+  }
+  return score;
+  //opponite       you
+  //A = rock = 1, X  
+  //B = paper = 2, Y
+  //C = scissors = 3, Z
+
+
+  // 1-3 = -2 rock vs scissors, loss
+  // 3-1 = 2 scissors vs rock, win
+
+  //1-2 = -1
+  //2-1 = 1
+
+  //index of there play -1 corresponds to our play in our play array
+  //them = [A, B, C]
+
+  //us = [X, Y, Z]
+  //if(us[them.indexOf(play)-1]===play)
+
 }
 
 run('day_01.test', day01Part1)
