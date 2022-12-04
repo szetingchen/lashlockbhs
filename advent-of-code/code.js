e7b3017b40f0fb103191917c2c7efff56c7e59b2
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-04 13:21:05 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -96,12 +96,12 @@ const day02Part2 = (str) =>{
   const goals = ["X", "Y", "Z"]
   for (let i = 0; i < games.length-1; i++) {
     let goal = games[i].substring(2, 3)
-    
+
+    console.log(goal)
     let thereplay = games[i].substring(0, 1)
 
     let ourplay = us[them.indexOf(thereplay)+1-goals.indexOf(goal)]
-    //console.log(thereplay)
-    console.log((them.indexOf(thereplay)-1 === -1 ? 2 : them.indexOf(thereplay)-1) + " " + us.indexOf(ourplay))
+
     if ((them.indexOf(thereplay)-1 === -1 ? 2 : them.indexOf(thereplay)-1) === us.indexOf(ourplay)) {
       score+=us.indexOf(ourplay)+1
       console.log("Loss " + thereplay + ", " + ourplay)
