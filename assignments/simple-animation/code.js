6f5225cc0b5c54bf27606a05c129d8de69a2d8b8
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-15 09:44:49 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -132,7 +132,7 @@ const drawFrame = (time) => {
     square1.drawShape();
     triangle1.drawShape();
 
-    console.log(squareobjectBound[2].y)
+    console.log(squareobjectBound[0].y)
     drawPoints(squareobjectBound)
 
     triangle1.rotation = triangle1.rotation * 1;
