107c721ff32e9fc4cdb245b014d28eb736fa15e9
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-15 09:25:51 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -126,7 +126,7 @@ const drawFrame = (time) => {
 
     square1.drawShape();
 
-    square1.rotation = square1.rotation * 1.05;
+    square1.rotation = square1.rotation * 1.005;
     next += 10;
     countFrame++;
   }
