76b551a758756872b7538d7c7f9b98d66d575d8c
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-15 09:21:52 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -111,8 +111,8 @@ const gravAttraction = (o1, o2) => {
 }
 
 
-const square1 = new Shape(shapeCordsSquare, 100, 100, 10, 5, 5, 5);
-const triangle1 = new Shape(trinaglesides, 100, 150, 0, 5, 5, 5);
+const square1 = new Shape(shapeCordsSquare, 100, 100, 0, 0, 5, 5, []);
+const triangle1 = new Shape(trinaglesides, 100, 150, 0, 0, 0, 5, []);
 
 
 let next = 0;
