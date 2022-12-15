bf3033c4498682f86d61d2d868554961a92d67d4
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-15 09:20:21 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -13,8 +13,8 @@ let trinaglesides = [
 ]
 
 const rotate = (cx, cy, x, y, angle) => {
-  let radians = (Math.PI / 180) * angle;
-  cos = Math.cos(radians),
+  let radians = (Math.PI / 180) * angle,
+    cos = Math.cos(radians),
     sin = Math.sin(radians),
     nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
     ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
