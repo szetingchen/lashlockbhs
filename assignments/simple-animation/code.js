60501806311e396dfbc2503b6e7641e6210d2737
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-16 11:29:36 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -91,6 +91,11 @@ class Shape {
 
       drawLine(cordSetStart[0], cordSetStart[1], cordSetEnd[0], cordSetEnd[1])
       
+      let xAddPerPix = cordSetEnd[0] - cordSetStart[0]
+      let yAddPerPix = cordSetEnd[1] - cordSetStart[1]
+
+      console.log(xAddPerPix + ", " + yAddPerPix)
+
       console.log(numOfSidePixels)
       console.log("Point Start: " + cordSetStart)
       console.log("Point End: " + cordSetEnd)
