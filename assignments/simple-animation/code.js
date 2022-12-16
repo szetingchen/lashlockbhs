799f3157a2405e3adb51fcf5f6f873e15eb1e5de
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-16 11:21:14 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -87,26 +87,10 @@ class Shape {
     for (let i = 0; i < 4; i++) {
       let cordSetStart = rotate(this.centerX, this.centerY, currX, currY, this.rotation)
       let cordSetEnd = rotate(this.centerX, this.centerY, currX + this.sidesCords[i].xAdd, currY + this.sidesCords[i].yAdd, this.rotation);
-      let numOfSidePixels = Math.sqrt(((cordSetStart[0]-cordSetEnd[0]) ** 2) + ((cordSetStart[1]-cordSetEnd[1]) ** 2));
 
       drawLine(cordSetStart[0], cordSetStart[1], cordSetEnd[0], cordSetEnd[1])
       
-      let xAddPerPixel = (cordSetEnd[0]-cordSetStart[0])/numOfSidePixels;
-      let yAddPerPixel = (cordSetEnd[1]-cordSetStart[1])/numOfSidePixels;
 
-      console.log("Point Start: " + cordSetStart)
-      console.log("Point End: " + cordSetEnd)
-      
-
-
-
-      for(n = 0; n < numOfSidePixels; n++){
-        array.push({ "x": currX + (xAddPerPixel * n), "y":  currY + (yAddPerPixel * n) })
-
-      }
-      console.log("---")
-      currX = currX + this.sidesCords[i].xAdd;
-      currY = currY + this.sidesCords[i].yAdd;
     }
     return array
   }
