e6540f5df4de1e6920116ffad44ce6f89cafb450
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-16 10:16:50 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -87,8 +87,8 @@ class Shape {
     for (let i = 0; i < this.sidesCords.length; i++) {
       let cordSetStart = rotate(this.centerX, this.centerY, currX, currY, this.rotation)
       let cordSetEnd = rotate(this.centerX, this.centerY, currX + this.sidesCords[i].xAdd, currY + this.sidesCords[i].yAdd, this.rotation);
-      console.log((cordSetStart[0]-cordSetEnd[0]))
-      console.log((cordSetStart[1]-cordSetEnd[1]))
+      console.log((cordSetStart[0]-cordSetEnd[0])**2)
+      console.log((cordSetStart[1]-cordSetEnd[1])**2)
       console.log(Math.sqrt(100))
       let numOfSidePixels = Math.sqrt((cordSetStart[0]-cordSetEnd[0]) ** 2 + (cordSetStart[1]-cordSetStart[1]) ** 2);
       let xAddPerPixel = (cordSetStart[0]-cordSetEnd[0])/numOfSidePixels;
