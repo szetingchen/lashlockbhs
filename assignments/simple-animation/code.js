0d935f4511fa8ade594bce06c77e2a2268d624e2
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-16 09:23:02 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -119,7 +119,9 @@ const drawPoints = (array) => {
 const square1 = new Shape(shapeCordsSquare, 100, 100, 1, 5, 5, 5, []);
 const triangle1 = new Shape(trinaglesides, 100, 100, 1, 5, 5, 5, []);
 
+square1.drawShape();
 
+/*
 let next = 0;
 let countFrame = 0;
 const drawFrame = (time) => {
@@ -139,5 +141,5 @@ const drawFrame = (time) => {
 }
 
 animate(drawFrame);
-
+*/
 
