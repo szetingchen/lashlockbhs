59739286cbe0de7050fb01507ee1d54e2601dacc
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-16 10:54:43 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -128,7 +128,7 @@ const triangle1 = new Shape(trinaglesides, 100, 100, 1, 5, 5, 5, []);
 //square1.drawShape();
 //console.log(square1.getBoundOfObject())
 drawPoints([{"x" : square1.centerX, "y" : square1.centerY}], "black")
-drawPoints([{"x" : square1.x, "y" : square1.y}], "black")
+drawPoints([{"x" : square1.x, "y" : square1.y}], "green")
 
 drawPoints(square1.getBoundOfObject(), "red")
 
