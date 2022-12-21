7996dcec60a8039470a7b64032b1bde1c068bce6
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-20 22:45:43 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -159,7 +159,7 @@ const drawFrame = (time) => {
     const squareObjectBound = square1.getBoundOfObject();
     const triangleObjectBound = triangle1.getBoundOfObject();
 
-    console.log(collisions([square1, triangle1]))
+    console.log(collisions([square1, triangle1]).length)
 
     drawPoints(squareObjectBound, "red")
     drawPoints(triangleObjectBound, "red")
