7f24a6d09d7af065a227a5e11a853ef6ab78a3f2
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-22 00:20:59 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -150,7 +150,7 @@ const triangle1 = new Shape(trinaglesides, 140, 100, 0, 5, 5, 5, []);
 
 let next = 0;
 let countFrame = 0;
-let timetilupdate = 100 //10 milaseconds between each frame
+let timetilupdate = 2000 //milaseconds between each frame
 const drawFrame = (time) => {
   if (time > next) {
 
