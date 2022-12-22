c957f49b7522bb7967e7b8126f6551bafbfb477e
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2022-12-22 00:26:04 -0800

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -144,8 +144,8 @@ const collisions = (shapes) => {
   }
   return collisionPoints;
 }
-const square1 = new Shape(shapeCordsSquare, 100, 90, 0, 5, 5, 5, []);
-const triangle1 = new Shape(trinaglesides, 45, 100, 0, 5, 5, 5, []);
+const square1 = new Shape(shapeCordsSquare, 90, 90, 0, 5, 5, 5, []);
+const triangle1 = new Shape(trinaglesides, 110, 100, 0, 5, 5, 5, []);
 
 
 let next = 0;
